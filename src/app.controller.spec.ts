2c35fcdc469c3a7e424bf25dc5f1dc4a96007e49
yeongbinim <yeim.dev@gmail.com>
2024-01-09 12:16:01 +0900

    test6

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -18,5 +18,8 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+    it('fail', () => {
+      expect(appController.getHello()).toBe('Hello World2');
+    });
   });
 });
