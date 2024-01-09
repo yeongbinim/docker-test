2184f6c49039daa6391ad29e2cbde06ddb203313
yeongbinim <yeim.dev@gmail.com>
2024-01-09 12:28:22 +0900

    test7-3

--- BE/test/app.e2e-spec.ts
+++ BE/test/app.e2e-spec.ts
@@ -14,7 +14,6 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
